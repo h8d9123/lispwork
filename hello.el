313d004364d5723e18a58d9e5ad6167c3de1bcb9
wanghz <h8d9123@163.com>
2019-05-26 21:35:56 +0800

    test-macro

--- hello.el
+++ hello.el
@@ -1,22 +1,28 @@
 (defun say-hello ()
   (print "Hello World !"))
 
-(defun max-num (a b)
+(defun num-max (a b)
   "return bigger number between a and b"
   (if (> a b) a b))
 
-(defun min-num (a b)
+(defun num-min (a b)
   "return smaller number between a and b"
   (if (> a b) a b))
+
+(defmacro ifnot (expr &rest body)
+  `(if (not ,expr) ,@body))
+
+(defmacro whennot (expr &rest body)
+  `(when (not ,expr) (progn ,@body)))
 
 (defun list-max (lst)
   "return the max number in lst"
   (if (null (cdr lst))
       (car lst)
-  (max-num (car lst) (list-max (cdr lst)))))
+  (num-max (car lst) (list-max (cdr lst)))))
 
 (defun list-min (lst)
   "return the min number in lst"
   (if (null (cdr lst))
       (car lst)
-  (min-num (car lst) (list-max (cdr lst)))))
+  (num-min (car lst) (list-max (cdr lst)))))
